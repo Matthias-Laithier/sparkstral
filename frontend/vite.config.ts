6c4f89249da2e656d8778bf542c86c9cbe4e7496
Matthias Laithier <matthias.laithier@epita.fr>
2026-04-24 12:32:44 +0200

    fix(frontend: compose.yaml): backend host env var

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: process.env.BACKEND_URL ?? "http://localhost:8000",
+				target: process.env.VITE_BACKEND_URL,
 				changeOrigin: true,
 			},
 		},
